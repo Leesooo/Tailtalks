520cb7c5bfc0db2dc3ff14931a3d4876d6eb91ce
leesooo <mdlclacio@tip.edu.ph>
2026-04-14 10:15:00 +0800

    pls pls by sabrina

--- setup_db.php
+++ setup_db.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$dbname = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+$host = getenv('MYSQLHOST') ?: 'monorail.proxy.rlwy.net';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: 'oLIPUFqIVgnLFvaMLPPqWuReumRzpbxd';
+$dbname = getenv('MYSQLDATABASE') ?: 'railway';
+$port = (int) getenv('MYSQLPORT');
 
 $conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 if (!$conn) die("Connection failed: " . mysqli_connect_error());
